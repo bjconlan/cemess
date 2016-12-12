631930e260ffb9e58074a75341b6641e4e5456bd
Benjamin Conlan <bjconlan@hotmail.com>
2016-12-12 23:51:21 +1100

    added basic builder support

--- src/cemess/client/core.cljs
+++ src/cemess/client/core.cljs
@@ -1,42 +1,67 @@
 (ns cemess.client.core
   (:require [com.stuartsierra.component :as component]
-            [datascript.core :as d]))
+            [datascript.core :as d]
+            [goog.dom :as dom]))
 
-(defrecord DatascriptConnection [options]
+;https://www.w3.org/TR/dom/#element
+(def seed [{:db/id -1
+            :node/value "foobar"}
+           {:db/id -2
+            :node/child-nodes [{:db/id -1}]
+            :element/id "test"
+            :element/tag-name dom/TagName.DIV
+            :element/attributes {:attribute/name "style" :attribute/value "color: red"}}])
+(def conn (d/create-conn {:element/attributes {:db/cardinality :db.cardinality/many
+                                               :db/valueType :db.type/ref
+                                               :db/isComponent true}
+                          :element/id         {:db/unique :db.unique/identity}
+                          :node/child-nodes   {:db/cardinality :db.cardinality/many                                     ;https://www.w3.org/TR/dom/#node
+                                               :db/valueType :db.type/ref}}))
+
+(d/transact! conn seed)
+
+(into {} (d/entity @conn [:element/id "test"]))
+
+(defn createNode [{:keys [:element/tag-name :element/id :element/attributes :node/child-nodes :node/value] :as node}]
+  (if tag-name
+    (let [el (dom/createElement tag-name)]
+      (some->> id (.setAttribute el "id"))
+      (doseq [{:keys [:attribute/name :attribute/value]} attributes]
+        (.setAttribute el name value))
+      (doseq [node child-nodes]
+        (.appendChild el (createNode (into {} node))))
+      el)
+    (some-> value (dom/createTextNode))))
+
+(dom/append js/document.body (createNode (d/entity @conn [:element/id "test"])))
+
+#_(defrecord App [options dom-conn data-conn]
   component/Lifecycle
   (start [this]
-    (if (:instance this)
-      this
-      (assoc this :instance (d/create-conn options))))
-  (stop [this]
-    (assoc this :instance nil)))
-
-(defrecord App [options datascript-connection]
-  component/Lifecycle
-  (start [this]
-    (let [conn (:instance datascript-connection)]
-      (prn (d/transact! conn [{:maker/name "Honda" :maker/country "Japan"}]))
+    #_(let [conn data-conn]
+      (d/transact! conn [{:maker/name "Honda" :maker/country "Japan"}])
       (d/transact! conn [{:db/id         -1
                           :maker/name    "BMW"
                           :maker/country "Germany"}
                          {:car/maker  -1
                           :car/name   "i525"
                           :car/colors ["red" "green" "blue"]}])
-      (d/q '[:find ?e ?name
-             :where
-             [?e :maker/name "BMW"]
-             [?c :car/maker ?e]
-             [?c :car/name ?name]]
-           @conn)))
+      (prn (d/q '[:find ?e ?name
+                  :where
+                  [?e :maker/name "BMW"]
+                  [?c :car/maker ?e]
+                  [?c :car/name ?name]]
+                @conn))))
   (stop [this] this))
 
-(defn context [{:keys [schema]}]
+#_(defn- context [_]
   (component/system-map
-    :datascript-connection (map->DatascriptConnection {:options schema})
-    :component-registry {:test []}
-    :app (component/using (map->App {:options nil}) [:datascript-connection])))
+    :dom-conn (d/conn-from-datoms [{:maker/name "Honda" :maker/country "Japan"}])
+    :data-conn (d/conn-from-datoms [] {:car/maker {:db/type :db.type/ref}
+                                       :car/colors {:db/cardinality :db.cardinality/many}})                                                                                   ;pretty generic name, perhaps 'content' or 'customizations'
+    :component-registry {}
+    :app (component/using (map->App {:options nil}) [:dom-conn :data-conn])))
 
-(def system (context {:schema {:car/maker {:db/type :db.type/ref}
-                               :car/colors {:db/cardinality :db.cardinality/many}}}))
+#_(def system (context {}))
 
-(component/start system)
+#_(component/start system)
